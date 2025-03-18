ac612257c78bc9742aaf5407af02d9308683044e
Jon Jablonski <jjablonski@library.ucsb.edu>
2025-03-18 14:17:24 -0700

    fixing paths and commenging out Gaviota sheet.

--- scripts/map_11.r
+++ scripts/map_11.r
@@ -3,24 +3,25 @@
 
 library(terra)
 
-dibblee_gol <- rast("07gGoleta/DB0007.tif")
+dibblee_gol <- rast("source_data/07gGoleta/DB0007.tif")
 dibblee_gol <- rectify(dibblee_gol, method="bilinear")
 dibblee_gol
 
 plot(dibblee_gol)
 plotRGB(dibblee_gol)
 
-dibblee_gav <- rast("16gSolvangGaviota/DB0016.tif")
-dibblee_gav <- rectify(dibblee_gav, method="bilinear")
+# oops, we lost this file somewhere
+#dibblee_gav <- rast("source_data/16gSolvangGaviota/DB0016.tif")
+#dibblee_gav <- rectify(dibblee_gav, method="bilinear")
 
-plot(dibblee_gav)
-plotRGB(dibblee_gav)
-collar <- read.delim("07gGoleta/DB0007.tif.txt", sep="\t", header = FALSE)
-str(collar)
-collar_df <- as.data.frame(collar)
-str(collar_df)
+#plot(dibblee_gav)
+#plotRGB(dibblee_gav)
+#collar <- read.delim("source_data/07gGoleta/DB0007.tif.txt", sep="\t", header = FALSE)
+#str(collar)
+#collar_df <- as.data.frame(collar)
+#str(collar_df)
 
-collar_vect <- vect(collar)
+#collar_vect <- vect(collar)
 ## Jon is thinking this is a georeferencing table -- not a definition of the collar
 
 # Crop extent from lat/long on the map corners:
