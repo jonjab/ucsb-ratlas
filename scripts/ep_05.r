ff4e296104ef97de3467043fedbe3d71c291e001
Jon Jablonski <jonjab@ucsb.edu>
2025-03-25 11:41:36 -0700

    making episode 5 match the canonical more

--- scripts/ep_05.r
+++ scripts/ep_05.r
@@ -1,14 +1,13 @@
 # episode 5
-# multi-band rasters
+# Work with Multi-Band Rasters
 # ie: color
+# https://datacarpentry.github.io/r-raster-vector-geospatial/05-raster-multi-band-in-r.html
 
 # clean the environment and hidden objects
 rm(list=ls())
 
-
 library(terra)
 library(tidyverse)
-# library(sf)
 library(raster)
 
 current_episode <- 5
@@ -24,38 +23,93 @@ gg_labelmaker <- function(plot_num){
   current_ggplot <<- plot_num
   return(plot_text)
 }
+
 # every ggtitle should be:
 # ggtitle(gg_labelmaker(current_ggplot+1))
 # end automagic ggtitle           #######
 
 
+# Getting Started with Multi-Band Data in R
+######################
+
+# load just 1 layer.
+west_campus_1ft <- rast("source_data/cirgis_1ft/w_campus_1ft.tif", lyrs = 1)
+# it draws realslow. do let's downsample
+# as in episode 12
+west_campus_4ft <- aggregate(west_campus_1ft, fact = 4)
+
+west_campus_df <- as.data.frame(west_campus_4ft, xy = TRUE)
+str(west_campus_df)
+
+ggplot() +
+  geom_raster(data = west_campus_df,
+              aes(x = x, y = y, alpha = w_campus_1ft_1)) + 
+  coord_quickmap()
 
 
 
-# do we still need raster in this lesson?
-# yes. for now. 
-natural_color <- raster("source_data/cirgis_1ft/w_campus_1ft.tif")
-natural_color_terra <- rast("source_data/cirgis_1ft/w_campus_1ft.tif")
+# Import A Specific Band
 
 
 
-#This is Planet
-planet_NCOS <- rast("source_data/planet/planet/2025-01-12_SkySat/20250112_185234_35_24f9_3B_AnalyticMS_8b_clip.tif")
+
+# Raster Stacks in R
+######################
+
+# if you don't speficy, you get all the bands
+
+west_campus_1ft <- rast("source_data/cirgis_1ft/w_campus_1ft.tif")
+# it draws realslow. do let's downsample
+# as in episode 12
+west_campus_4ft <- aggregate(west_campus_1ft, fact = 4)
+
+# this shows there are 5 bands
+west_campus_4ft
+
+# can send just 1 band to stdout:
+west_campus_4ft[[2]]
+
+west_campus_df <- as.data.frame(west_campus_4ft, xy = TRUE)
+str(west_campus_df)
 
 
-# can do the quickplot, it's very mysterious
-plot(planet_NCOS)
-plot(natural_color)
-plot(natural_color_terra)
+# Create A Three Band Image
+######################
+# https://datacarpentry.github.io/r-raster-vector-geospatial/05-raster-multi-band-in-r.html#create-a-three-band-image
+
+# plotRGB is for stacks
+plotRGB(west_campus_4ft)
+
+plotRGB(west_campus_4ft, stretch = "lin")
+plotRGB(west_campus_4ft, stretch = "hist")
 
 
-str(natural_color)
-class(natural_color)
+plotRGB(west_campus_4ft,
+        r = 1,
+        g = 2, 
+        b = 3)
 
-# outputting it to stdout shows how
-# many bands (5 and 8).
-natural_color
-planet_NCOS
+plotRGB(west_campus_4ft,
+        r = 2,
+        g = 3, 
+        b = 4)
+
+plotRGB(west_campus_4ft,
+        r = 3,
+        g = 4, 
+        b = 5)
+
+plotRGB(natural_color_stack,
+        r = 3,
+        g = 2, 
+        b = 1)
+
+
+
+
+# plotRGB(NCOS, r=1, g=2, b=3)
+
+
 
 # brick is from raster
 # a brick is a new class for us
@@ -64,9 +118,6 @@ natural_color_brick <- brick("source_data/cirgis_1ft/w_campus_1ft.tif")
 natural_color_brick
 
 plotRGB(natural_color_brick)
-
-
-# we also have raster stack
 natural_color_stack <- stack("source_data/cirgis_1ft/w_campus_1ft.tif")
 
 planet_NCOS_stack <- stack("source_data/planet/planet/2025-01-12_SkySat/20250112_185234_35_24f9_3B_AnalyticMS_8b_clip.tif")
@@ -78,36 +129,6 @@ str(natural_color)
 # has 8 bands
 planet_NCOS_stack
 
-# plotRGB(NCOS, r=1, g=2, b=3)
-
-
-# plotRGB is for stacks
-plotRGB(natural_color_stack)
-
-plotRGB(natural_color_stack, stretch = "lin")
-plotRGB(natural_color_stack, stretch = "hist")
-
-
-plotRGB(natural_color_stack,
-        r = 1,
-        g = 2, 
-        b = 3)
-
-plotRGB(natural_color_stack,
-        r = 2,
-        g = 3, 
-        b = 4)
-
-plotRGB(natural_color_stack,
-        r = 3,
-        g = 4, 
-        b = 5)
-
-plotRGB(natural_color_stack,
-        r = 3,
-        g = 2, 
-        b = 1)
-
 # the metadata shows 5 bands. How 
 # about nodata value?
 
@@ -116,21 +137,14 @@ plotRGB(natural_color_stack,
 # but I don't see NA's described
 describe("source_data/w_campus_1ft/w_campus_1ft.tif")
 
-# SpatRasterDataset
-# comes from terra
+# SpatRaster in R
+###################
+
+# SpatRasterDataset comes from terra
 natural_color_sds <- sds(natural_color_terra)
 
-# later we will stack up some SkySat imagery into a time series.
+
+# Challenge: What Functions Can Be Used on an R Object of a particular class?
+###################
 
 
-# ##################################
-# take 2
-# Alternate path preparing better for
-# episode 12
-# we can do this with SkySat!
-# SkySatPanSharp_B <- brick("source_data/SkySatCollect/20161210_185631_ssc3_u0001_pansharpened_clip.tif")
-# SkySatPanSharp_S <- stack("source_data/SkySatCollect/20161210_185631_ssc3_u0001_pansharpened_clip.tif")
-
-
-# stdout shows 4 bands
-# plotRGB(SkySatPanSharp_B, r=1, g=2, b=3, scale=1932)
