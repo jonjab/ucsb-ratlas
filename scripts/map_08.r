9bb56a825d82e4a86189502541f1839489503096
kristi-sara <ksliu@ucsb.edu>
2025-04-01 10:47:21 -0700

    add map 8, 8band image notes

--- scripts/map_08.r
+++ scripts/map_08.r
@@ -44,3 +44,10 @@ library(curl)
 # plnt_quota
 
 curl(url="https://api.planet.com/features/v1/ogc/my?format=api", open = "")
+
+#load an 8-band image: data_prep puts it here:
+#  source_data/UCSB_campus_23-24_psscene_analytic_8b_sr_udm2
+#plot out 3 different combinations:
+#  natural color
+#  false-color IR
+#  something pretty or odd
