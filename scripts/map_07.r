9dfac1c3ced196d648cba78f95ec453c7a03df3a
Jon Jablonski <jjablonski@library.ucsb.edu>
2025-04-17 17:20:54 -0700

    arranging map 7 to reflect current titles.

--- scripts/map_07.r
+++ scripts/map_07.r
@@ -4,36 +4,57 @@
 # and map 6, which is a version of map 1
 # 
 
+# clean the environment and hidden objects
+rm(list=ls())
+
 # Load magick package, which allows to make a montage of images
 library(magick)
+
+# set map number
+current_sheet <- 7
+# set ggplot counter
+current_ggplot <- 0
+
+
+
 
 # We'll be doing a 12inx9in image, which at 300 PPI is 3600x2700 pixels
 
 # Load maps 3, 4, 5, and 6 using the magick::image_read function
-map4 <- image_read("images/map3.png") # 3x4 inches = 900x1200 pixels
-map5 <- image_read("images/map4.png") # 3x4 in = 900x1200 px
-map6 <- image_read("images/map5.png") # 4x3 in = 1200x900 px
-map7 <- image_read("images/map6.0.png") # 12x9 in = 3600x1200 px
+map3 <- image_read("images/map3.png") # 3x4 inches = 900x1200 pixels
+map4 <- image_read("images/map4.png") # 3x4 in = 900x1200 px
+map5 <- image_read("images/map5.png") # 4x3 in = 1200x900 px
+map6 <- image_read("images/map6.0.png") # 12x9 in = 3600x1200 px
+
+image_read("images/map3.png")
+image_read("images/map4.png")
+image_read("images/map5.png")
+image_read("images/map6.0.png")
 
 # There are multiple ways to use the magick package to append images,
 # you could use image_append(), image_montage(), or image_composite()
 
-# Let's first make row number 1 or our atlas page, which combines maps 4, 5, and 6"
+
+
+# Let's first make row number 1 or our atlas page, which combines maps 3, 4, and 5"
 # This will have a geometry of 4800x1200px (12x4 inches), so each tile is 1600x1200
 
-map7_row1 <- image_montage(image = c(map4, map5, map6),
+map7_row1 <- image_montage(image = c(map3, map4, map5),
               tile = "3x1",
               geometry = "1600x1200",
               gravity = "center")
-map7_row1
+(map7_row1)
 
 # save that
 image_write(map7_row1, "final_output/map7_row_1.png", format = "png")
+# output it
+image_read("final_output/map7_row_1.png")
 
-map7 <- image_montage(image = c(map7_row1, map7),
+map7 <- image_montage(image = c(map7_row1, map6),
                       tile = "1x2",
                       geometry = "4800x1200")
-map7
+
 
 #save map7
 image_write(map7, "images/map7.png", format = "png")
+image_read("images/map7.png")
