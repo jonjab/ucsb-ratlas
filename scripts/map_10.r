5dad408beee0120e3d0cd15feb270760352ad6d2
Ken Howard <darrinkhoward@ucsb.edu>
2025-04-27 19:12:46 -0700

    map 10, ready to load bike-path file

--- scripts/map_10.r
+++ scripts/map_10.r
@@ -1,6 +1,7 @@
 # map 10
-
-
+library(terra)
+library(tidyverse)
+library(tidyterra)
 # clean the environment and hidden objects
 rm(list=ls())
 
@@ -20,3 +21,18 @@ gg_labelmaker <- function(plot_num){
 # every ggtitle() or labs() should be:
 # ggtitle(gg_labelmaker(current_ggplot+1))
 # end automagic ggtitle           #######
+
+# ---------- 1. read layers ----------
+bike_paths <- vect("") # replace with updated bike-path file
+creeks     <- vect("source_data/california_streams/California_Streams.shp")
+
+# ---------- 2. common CRS ----------
+creeks      <- project(creeks, bike_paths)
+
+# ---------- 3. intersection points ----------
+creek_bike_pts <- intersect(creeks, bike_paths)
+
+# ---------- 4. write shapefile ----------
+#  Do we output a new point shapefile as a result?
+out_file <- "output_data/creek_bike_intersections.shp"
+writeVector(creek_bike_pts, out_file, overwrite = TRUE)
